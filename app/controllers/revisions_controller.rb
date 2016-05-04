f955367174e5f0fabe320530540dfb6e8f7f7449
jonnorstrom <jon.norstrom@gmail.com>
2016-05-04 15:43:20 -0500

    migrations and models

--- app/controllers/revisions_controller.rb
+++ app/controllers/revisions_controller.rb
@@ -1,2 +1,29 @@
 class RevisionsController < ActionController::Base
+  def new
+    @revision = Revision.new
+    # render?
+  end
+
+  def create
+    @revision = Revision.new(params[:revision])
+
+    if @revision.save?
+      # do something
+    else
+      # render something
+    end
+  end
+
+  def index
+    @article = Article.find(params[:article_id])
+    @revisions = @article.revisions
+    # show all revisions for one speicific article
+    # render something
+  end
+
+  def show
+    @revision = Revision.find(params[:id])
+    # show one specific revision for any given article
+    # render something
+  end
 end

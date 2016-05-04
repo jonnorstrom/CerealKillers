428c5f2e5ec9c63ed6ef226aa8c64dc4b38400ac
Erica Lloyd <email": null>
2016-05-04 16:50:49 -0500

    Update model, migration

--- db/migrate/20160504211255_create_articles.rb
+++ db/migrate/20160504211255_create_articles.rb
@@ -2,6 +2,7 @@ class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
       t. string :title, null: false
+      t.integer :user_id, null: false
       t.timestamps null: false
     end
   end

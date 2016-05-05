f1d708ed0d3442ffdf4fded669d21ed123f54e93
jonnorstrom <jon.norstrom@gmail.com>
2016-05-05 10:26:44 -0500

    fix css

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20160505135259) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      null: false
-    t.integer  "user_id",    null: false
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
